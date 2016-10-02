86297dfd4d4ab56d92472d1196bcc77fc5486238
metalx1000 <git@filmsbykris.com>
2016-10-02 19:41:57 -0400

    added favicon

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+<!-- favicon -->
+  <?php include('fav.php');?>
+<!-- end favicon -->
+
   <script>
     $(document).ready(function(){
       $.get('list.php',function(data){
--- read.php
+++ read.php
@@ -7,6 +7,9 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- favicon -->
+  <?php include('fav.php');?>
+<!-- end favicon -->
 
   <style>
     body,html {
