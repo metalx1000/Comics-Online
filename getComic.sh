629da00b50d7716ff1d55ea5e6a343d70bfecbcd
metalx1000 <git@filmsbykris.com>
2016-10-02 19:23:35 -0400

    added some example comics

--- getComic.sh
+++ getComic.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 comicDIR="comics"
-title="$(grep "<head><title>" -A3 comic.tmp |tail -n2|dos2unix| sed -e 's/    //g' | sed ':a;N;$!ba;s/\n/ - /g'|sed 's/#//g')"
+title="$(grep "<head><title>" -A3 comic.tmp |head -n 4|tail -n2|dos2unix| sed -e 's/    //g' | sed -e ':a;N;$!ba;s/\n/ - /g' -e 's/#//g' -e 's/&/and/g')"
+
+echo "Creating list for $title"
+echo "Adding list to $comicDIR/$title.php"
 grep 'lstImages.push' comic.tmp|cut -d\" -f2 > "$comicDIR/$title.php"
+
+echo "Number of pages found $(wc -l "$comicDIR/$title.php")"
