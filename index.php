5cddfc96ee4cca088b5a2c4c5d0beec0f20c4f89
kris <kris@server1.catgreper.com>
2016-10-06 16:24:15 -0500

    read comics are now marked

--- index.php
+++ index.php
@@ -12,18 +12,73 @@
   <?php include('fav.php');?>
 <!-- end favicon -->
 
+  <style>
+    .read{
+      color: lightgrey;
+    }
+
+    #mainlist{
+      font-weight: bold;
+    }
+  </style>
   <script>
     var comics;
+    if (typeof(Storage) !== "undefined") {
+      // Code for localStorage/sessionStorage.
+      read = JSON.parse(localStorage.getItem("readComic"));
+    } else {
+        // Sorry! No Web Storage support..
+    }
+
+
     $(document).ready(function(){
       $.get('list.php',function(data){
         comics = data.split("\n").sort();
         comics.forEach(function(comic){
           if(comic != ""){
-            $("#mainlist").append('<a href="read.php?comic='+comic+'"><li class="list-group-item">'+comic+'</li></a>');
+            var x = "";
+            if(read != null){
+              read.forEach(function(l){
+                if(l == comic){
+                  x = "read";
+                }
+              });
+            }
+            $("#mainlist").append('<li class="list-group-item '+x+'">'+comic+'</li>');
           }
         });
       });
+
+      $("#mainlist").on("click","li",function(){
+        markRead(this);
+        window.location = "read.php?comic=" + this.innerHTML;
+      });
     });
+
+    function markRead(item){
+      var link = item.innerHTML;
+      if (typeof(Storage) !== "undefined") {
+        // Code for localStorage/sessionStorage.
+        if(read == null){
+          read = [link];
+        }else{
+          read.push(link);
+        }
+        read = sort(read);
+
+        localStorage.setItem("readComic",JSON.stringify(read));
+      } else {
+          // Sorry! No Web Storage support..
+      }
+
+    }
+
+
+    function sort(arr) {
+      return arr.sort().filter(function(el,i,a) {
+          return (i==a.indexOf(el));
+      });
+    }
   </script>
 </head>
 <body>
